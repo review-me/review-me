18d18c26f83a791910a95507bcf0b4e9f076cbc7
Tetchenko Dmitry <tet4enko@yandex-team.ru>
2015-04-10 22:04:28 +0300

    json samples

--- layouts/layout-index/layout-index.deps.js
+++ layouts/layout-index/layout-index.deps.js
@@ -1,5 +1,5 @@
 ({
     shouldDeps: [
-        {block : 'button', mods: {theme: 'islands',size: 'xl' }}
+        {block : 'button', mods: {theme: 'islands',size: 'l', view: 'action', type: 'link' }}
     ]
 });
